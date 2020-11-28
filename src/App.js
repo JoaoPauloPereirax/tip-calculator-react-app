93b4a953b9823127d5d8ceffad298e08a62a5dbf
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-11-28 05:19:37 -0300

    Projeto prático

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Title = styled.h1``;
+
+const Input = styled.input`
+
+`;
+
+function App(){
+
+   const [vconta, setVconta ] = useState(0);
+   const [percentual, setPercentual]= useState(0);
+
+ return <div className="gorj">
+    <Title>Calculadora de Gorjeta</Title>
+    <h2>Quanto deu a conta?</h2>
+    <Input type="number" value={vconta} onChange={(i)=>{setVconta(parseFloat(i.target.value))}}></Input>
+    <h2>Qual a porcentagem de gorjeta?</h2>
+    <Input type="number" value={percentual} onChange={(j)=>{setPercentual(parseFloat(j.target.value))}}></Input>
+   {
+     vconta>0 &&
+     <>
+      <p>Sub-total: R$ {vconta+(vconta*(percentual/100))}</p>
+      <p>Gorjeta(%{percentual}): R$ {vconta*(percentual/100)}</p>
+     </>
+   }
+ </div>;
 }
 
 export default App;
